ce7a2018514da66662c460b144736c6906b7bf93
Alhiefikri <Alhiepedia@gmail.com>
2024-07-27 22:32:48 +0800

    Menambahkan Paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque autem dolor enim repellendus dolorum consequuntur temporibus odio harum atque, alias explicabo accusantium iste quia magnam velit quisquam voluptatibus, aperiam consectetur.</p>
 </body>
 </html>
